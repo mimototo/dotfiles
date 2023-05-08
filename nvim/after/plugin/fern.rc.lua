a1d01933a078f5b2e609104348086524fe0ce98d
mimototo <mail@mimototo.jp>
2023-05-08 18:02:38 +0900

    docs: ✏️ add git config ignore files

--- nvim/after/plugin/fern.rc.lua
+++ nvim/after/plugin/fern.rc.lua
@@ -5,3 +5,5 @@ fern.setup {}
 
 -- Use the <Leader>f keymap to open fern file explorer
 vim.keymap.set('n', 'ff', ':Fern . -reveal=% -drawer -toggle -width=40<CR>', { noremap = true, silent = true })
+
+--test
