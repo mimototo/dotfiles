50f3700fff1de9551c813a522f1446ee4846930a
mimototo <mail@mimototo.jp>
2023-05-30 18:01:54 +0900

    feat: 🎸 feat
    feat

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -77,6 +77,7 @@ nvim_lsp.flow.setup {
 nvim_lsp.tsserver.setup {
   on_attach = on_attach,
   filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  -- filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
   cmd = { "typescript-language-server", "--stdio" },
   capabilities = capabilities
 }
